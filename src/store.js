c70c7a066bde1db7364ebb25d17e699e84d48db5
phi12ip <coderphilip@gmail.com>
2020-08-10 13:46:52 -0400

    setup

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import thunk from 'redux-thunk';
+import { composeWithDevtools } from 'redux-devtools-extension';
 import { todos } from './todos/reducers';
 
 const reducers = {
@@ -19,6 +21,9 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const configureStore = () =>
     createStore(
         persistedReducer,
+        composeWithDevetools(
+            applyMiddleware(thunk)
+        ),
         window.__REDUX_DEVTOOLS_EXTENSION__ &&
         window.__REDUX_DEVTOOLS_EXTENSION__(),
     );
