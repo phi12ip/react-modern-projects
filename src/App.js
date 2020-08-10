e037130f36a7e16bdf5dc154523ce0ece67f74fc
phi12ip <coderphilip@gmail.com>
2020-08-10 10:20:54 -0400

    added hot loading to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {hot} from 'react-hot-loader';
 import './App.css';
-import 
+
 
 const App = () => (
     <div className="App">
         <h1>Hello world!</h1>
     </div>
 
-)
+);
 
 
-export default App;
+export default hot(module)(App);
